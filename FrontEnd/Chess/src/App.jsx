ba05f3d6e17ad1f88770825167bb991aa5b999e8
HellBats <vishalgoyal452003@gmail.com>
2024-02-28 23:43:02 +0530

    Created very very better Layout and arranged chess pieces in their default positions

--- FrontEnd/Chess/src/App.jsx
+++ FrontEnd/Chess/src/App.jsx
@@ -1,62 +1,81 @@
 
+import { useState } from 'react';
 import './App.css'
 
 function App() {
 
   let chess_board = [];
+  let pieces = [];
   {
     for(let i=7;i>=0;i--)
     {
       let row = [];
       for(let j=0;j<8;j++)
       {
-          row.push(String.fromCharCode(65+j)+(i+1))
+          row.push([String.fromCharCode(65+j)+(i+1)])
       }
       chess_board.push(row);
     }
-    console.log(chess_board);
   }
+  const [board,setBoard] = useState(chess_board);
   return (
     <div>
-      <div><h1 id='Heading'>Chess</h1></div>
+      <TopBar/>
       <div className='Layout'>
-      <BoardGenerator board={chess_board}></BoardGenerator>
+      <BoardGenerator board={board}></BoardGenerator>
       </div>
     </div>
   )
 }
 
+function TopBar()
+{
+  return (
+    <div className='TopBar'>
+        <img id="Icon" src="src/assets/Icon.jpg"></img>
+        <div id="Heading">ZoanFruit</div>
+    </div>
+  )
+}
 
 function BoardGenerator({board}) {
-    let iterator = 0;
-    let pass = 0;
+  let pieces = ['br','bn','bb','bq','bk','bb','bn','br'
+                ,'bp','bp','bp','bp','bp','bp','bp','bp'
+                ,'wp','wp','wp','wp','wp','wp','wp','wp'
+                ,'wr','wn','wb','wq','wk','wb','wn','wr'];
+  let iterator = 0;
+  let pass = 0;
+  let row = 0;
+  let color = false;
   return (
     <div className="container">
-      {
-        board.map(rows=>
-          {
-            let row = [];
-      
-            rows.map(box=>
+      {board.map(row=>
+          row.map((box)=>
             {
-              if(iterator%2==0)
+              if(iterator<16)
               {
-                row.push(<img id = {box} src="/src/assets/Green_box.png"></img>);
+                let src = "src/assets/"+pieces[iterator]+".png";
+                iterator++;
+                return <img className= "piece" src={src}/>;
+              }
+              if(pass<32)
+              {
+                pass++
+                return <img/>;
               }
               else
               {
-                row.push(<img id = {box} src="/src/assets/White_box.png"></img>);
+                let src = "src/assets/"+pieces[iterator]+".png";
+                iterator++;
+                return <img className= "piece" src={src}/>;
               }
-              iterator++
-            })
-            pass++;
-            if(pass%2==0){iterator=0;}
-            else{iterator=1;}
-            return row;
-          })
+            }
+          )
+      )
       }
     </div>
   );
 }
+
 
 export default App
