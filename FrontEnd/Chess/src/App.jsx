e89c81ed33fa659a58271f8fb239a65dffad3772
HellBats <vishalgoyal452003@gmail.com>
2024-03-03 01:09:50 +0530

    Pieces can move freely on any position without any bugs and change the logic of how piece move from one position to another

--- FrontEnd/Chess/src/App.jsx
+++ FrontEnd/Chess/src/App.jsx
@@ -3,20 +3,8 @@ import BoardGenerator from './Components/DefaultBoardLayout';
 import TopBar from './Components/TopBar';
 import './App.css'
 
-function App() {
 
-  let chess_board = [];
-  {
-    for(let i=8;i>0;i--)
-    {
-      let row = [];
-      for(let j=1;j<9;j++)
-      {
-          row.push([i.toString()+j.toString()])
-      }
-      chess_board.push(row);
-    }
-  }
+function App() {
   return (
     <div>
       <TopBar/>
--- FrontEnd/Chess/src/Components/DefaultBoardLayout.jsx
+++ FrontEnd/Chess/src/Components/DefaultBoardLayout.jsx
@@ -1,7 +1,35 @@
-import { useState,useEffect } from "react";
+import { useState,useEffect,useRef } from "react";
+
 
 export default function BoardGenerator() {
+  //This reference hook and function is used to detect postion of click and then row and column 
+  //are calculated and then sends to Move Function for further processing
+  const divRef = useRef(null);
+  const handleClick = (event) => {
+    const rect = divRef.current.getBoundingClientRect();
+      const clickX = event.clientX-rect.left;
+      const clickY = event.clientY-rect.top;
+      if(clickX>0 && clickX<720 && clickY>0 && clickY<720)
+      {
+        const board_length = 720;
+        const blockX = Math.floor(clickX/board_length*8+1);
+        const blockY = Math.floor(clickY/board_length*8+1);
+        Move({ row: blockX, column: blockY, move, setMove,position,setPosition });
+      }
+  };
+  //This hook detect clicks
+  useEffect(() => {
+    document.addEventListener("click", handleClick);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, []);
+
+  // Move State is used to keep check of clicks happening on boards
   const [move,setMove] = useState([]);
+  //Position state is used to maintain a state of Chess board
   const [position,setPosition] = useState(
     [
       [['br',1,1], ['bn',1,2], ['bb',1,3], ['bq',1,4], ['bk',1,5], ['bb',1,6], ['bn',1,7], ['br',1,8]],
@@ -14,17 +42,24 @@ export default function BoardGenerator() {
       [['wr',8,1], ['wn',8,2], ['wb',8,3], ['wq',8,4], ['wk',8,5], ['wb',8,6], ['wn',8,7], ['wr',8,8]]
     ]
   );
+  let key = 0;
     return (
-      <div className="container" >
-         {position.map(row=>
+      <div className="container" ref={divRef}>
+         {
+         position.map(row=>
             row.map((box)=>
               {
+                //This rank and column use values from position and so exchange of position is necessary
                 let rank = box[1];
                 let column = box[2];
-                let src = "src/assets/"+position[rank-1][column-1][0]+".png";
-                return <Piece key={rank*8+box[1]*8+box[2]} src={src}
-                row_={rank} column={column} position={position} 
-                setPosition={setPosition} move={move} setMove={setMove}></Piece>
+                key++;
+                if(position[rank-1][column-1][0]!='tr')
+                {
+                  let src = "src/assets/"+position[rank-1][column-1][0]+".png";
+                  return <Piece  key={key} src={src}
+                  row_={rank} column={column} position={position} 
+                  setPosition={setPosition} move={move} setMove={setMove}></Piece>
+                  }
               }
             )
         )
@@ -33,39 +68,71 @@ export default function BoardGenerator() {
     );
   }
 
-function Piece({src,row_,column,position,setPosition,move,setMove})
+function Piece({src,row_,column,position})
   {
-    function Move(){  
-      let cords = [...move,[row_,column]];
-      setMove(cords);
+    //This is the Piece Component that renders according to position state
+    const pieceType = position[row_ - 1][column - 1][0];
+    if (pieceType !== 'tr' && pieceType !== undefined) {
+      return (
+        <img
+          className="piece"
+          src={src}
+          style={{
+            gridRow: position[row_ - 1][column - 1][1],
+            gridColumn: position[row_ - 1][column - 1][2],
+          }}
+        />
+      );
+    } else {
+      return null; // or render an empty component, depending on your preference
     }
-
-    useEffect(() => {
-      if (move.length == 2) {
-        MovetoPiece(move, setMove, position, setPosition);
-        setMove([]);
-      }
-    }, [move, position, setMove, setPosition]);
-
-
-    return (
-    <>
-    <img className= "piece" src={src}
-    style={{gridRow:position[row_-1][column-1][1],gridColumn:position[row_-1][column-1][2]}} onClick={()=>Move()}/>
-    </>
-    );
   }
 
-function MovetoPiece(move,setMove,position,setPosition)
+function Move({ row, column, move, setMove,position,setPosition}) {  
+  //This function check for no of click and if click was on a piece or an empty space and also clear
+  //move after two valid clicks  
+  setMove(prevMove => {
+      const cords = [...prevMove, [column, row]];
+      if(cords.length==1)
+      {
+        if(position[column-1][row-1][0]!='tr')
+        {
+          return cords;
+        }
+        else
+        {
+          return [];
+        }
+      }
+      if(cords.length==2)
+      {
+        MovetoPiece({cords,position,setPosition});
+        return [];
+      }
+    });
+  }
+  
+
+
+function MovetoPiece({cords,setPosition})
 {
-  let new_position = [...position];
-  let piece1_row = move[0][0];
-  let piece1_column = move[0][1];
-  let piece2_row = move[1][0];
-  let piece2_column = move[1][1];
-  new_position[piece1_row-1][piece1_column-1][1] = piece2_row;
-  new_position[piece1_row-1][piece1_column-1][2] = piece2_column;
-  new_position[piece2_row-1][piece2_column-1][1] = piece1_row;
-  new_position[piece2_row-1][piece2_column-1][2] = piece1_column;
-  setPosition(new_position);
+  setPosition(prevPosition=>
+    {
+    // Getting the privous state from position and trying to update it into new state by getting 
+    // cords for Move function 
+    let new_position = [...prevPosition];
+    let piece1_row = cords[0][0];
+    let piece1_column = cords[0][1];
+    let piece2_row = cords[1][0];
+    let piece2_column = cords[1][1];
+  
+    //Updating the row and column value of piece to be moved
+    new_position[piece1_row - 1][piece1_column - 1][1] = piece2_row;
+    new_position[piece1_row - 1][piece1_column - 1][2] = piece2_column;
+    //Exchanging the postion of piece to the position to be moved to because when rendered
+    // mapping happens and it takes row and columns value to check if image is 'tr' or not
+    //and so if position in array not excjanged then it will get ignored
+    new_position[piece2_row - 1][piece2_column - 1] = new_position[piece1_row - 1][piece1_column - 1];
+    return new_position;
+  });
 }
